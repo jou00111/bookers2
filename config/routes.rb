11e8c4e2576b7ec3a483f7f41f944d17d634d75f
EC2 Default User <ec2-user@ip-172-31-8-165.ap-northeast-1.compute.internal>
2024-09-17 04:56:02 +0000

    [start] study 9/17

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to:'homes#top'
+  resources :books,　only: [:new,  :create, :edit, :show, :destroy]
+   resources :users,  only: [:edit, :show]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+
+
+const webpack =require('webpack')
+
+environment.plugins.prepend(
+
+  'Provide',
+
+ new webpack.ProvidePlugin({
+
+   $: 'jquery/sec/jquery',
+
+   jQuery: 'jquery/src/jquery',
+
+   Popper: 'popper.js'
+
+ })
+
+  )
+
+
